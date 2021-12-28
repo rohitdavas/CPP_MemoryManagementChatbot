ce5672409f23acbf1be6ae56fa492ca26060adb9
rohitdavas <davasrohit@gmail.com>
2021-12-28 13:39:00 +0000

    task 2 done

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -118,8 +118,10 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     ////
 
     // create chat logic instance
-    _chatLogic = new ChatLogic(); 
-
+     std::cout << "Creating a unique ptr in ChatPanel Dialog constructor"<<std::endl; 
+      
+    //_chatLogic = new ChatLogic(); 
+	_chatLogic = std::make_unique<ChatLogic>(); 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
 
@@ -135,8 +137,9 @@ ChatBotPanelDialog::~ChatBotPanelDialog()
     //// STUDENT CODE
     ////
 
-    delete _chatLogic;
-
+    //delete _chatLogic;
+	std::cout << "the smart pointer destroys itself." <<std::endl; 
+  
     ////
     //// EOF STUDENT CODE
 }
